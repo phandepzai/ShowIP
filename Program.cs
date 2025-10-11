b9189ce5f9ba850898cb3d519fa636f3d800106b
nongvanphan <60602558+phandepzai@users.noreply.github.com>
2025-10-11 18:22:17 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
+using System.Threading; // Thêm using này để sử dụng Mutex
 
 namespace Show_Infomation
 {
@@ -8,9 +9,30 @@ namespace Show_Infomation
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            // Tạo Mutex với tên unique (global)
+            bool createdNew;
+            using (Mutex mutex = new Mutex(true, "ShowInformationSingleInstanceMutex", out createdNew))
+            {
+                if (createdNew)
+                {
+                    // Nếu Mutex mới được tạo (chưa có instance nào chạy), tiếp tục chạy ứng dụng
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    // Nếu Mutex đã tồn tại (instance khác đang chạy), hiển thị thông báo và thoát
+                    MessageBox.Show(
+                        "Ứng dụng đã đang chạy. Không thể mở thêm phiên bản mới.",
+                        "Thông báo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+                    // Thoát ứng dụng ngay lập tức
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
